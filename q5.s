669494948a53c9ab0791ff5cd5af7883e68a6c89
Connor Morrison <tvi340@usask.ca>
2025-01-22 23:54:39 -0600

    Continued implementation of q5.s
    - Intialize counter for counting instances of abs(42)
    - Intialize loop counter for loop
    - Intital loop with system prompt, and readInt
    - loop_counter and abs_counter 'functions' created
    - Exit command moved to 'done'

--- q5.s
+++ q5.s
@@ -7,8 +7,8 @@
 # Read-only data section
 	.section .rodata
 prompt_n:       .asciz "Enter the number of integers: \n"
-prompt_value:     .asciz "Enter an integer: \n"
-output_message: .asciz "Count of numbers with absolute value 42: \n"
+prompt_value:   .asciz "Enter an integer: \n"
+output_message: .asciz "Count of numbers with value 42: \n"
 
 # Code section
 	.section .text
@@ -20,6 +20,41 @@ _start:
 	li a7, SYS_printStr
 	ecall
 
+	# Read N
+	li a7, SYS_readInt
+	ecall
+	mv s0, a0	# Move value from a0 to s0
+
+	# Initialize counter for loop
+	mv s1, zero	# Intialize s1 to 0; s1 = count of numbers with value 42
+	mv s2, zero	# Intialize s2 to 0; loop counter
+
+loop:
+	# Check if processed N numbers
+	beq s2, s0, done
+	
+	la a0, prompt_value
+	li a7, SYS_printStr
+	ecall
+
+	li a7, SYS_readInt
+	ecall
+
+	abs t0, a0	# Get the absolute value of value in a0, then store in t0
+	li t1 42
+	beq t0, t1, abs_counter
+
+loop_counter:
+	addi s2, s2, 1
+	j loop
+	
+
+abs_counter:
+	addi s1, s1, 1
+	j loop
+
+
+done:
 	# Exit the program
 	li a0, 0
 	li a7, SYS_exit
