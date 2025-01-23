ceb4a671ee7c99b05f9212f26103d6e8f6e1af39
Connor Morrison <tvi340@usask.ca>
2025-01-23 01:28:01 -0600

    Updated loop logic, printing format, and error handling
    - Updated and simplifed loop logic to handle negation
    - Updated printing format to add newline
    - Add error handing to exit program if non-positive number entered at
      beginning

--- q5.s
+++ q5.s
@@ -6,9 +6,10 @@
 
 # Read-only data section
 	.section .rodata
-prompt_n:       .asciz "Enter the number of integers: \n"
-prompt_value:   .asciz "Enter an integer: \n"
-output_message: .asciz "Count of numbers with value 42: \n"
+prompt_n:       .asciz "Enter the number of integers: "
+prompt_value:   .asciz "Enter an integer: "
+output_message: .asciz "Count of numbers with absolute value 42: "
+newline:        .asciz "\n"
 
 # Code section
 	.section .text
@@ -25,37 +26,56 @@ _start:
 	ecall
 	mv s0, a0	# Move value from a0 to s0
 
-	# Initialize counter for loop
+	# Exit if N <= 0
+	blez s0, exit
+
+	# Initialize counters
 	mv s1, zero	# Intialize s1 to 0; s1 = count of numbers with value 42
 	mv s2, zero	# Intialize s2 to 0; loop counter
 
 loop:
 	# Check if processed N numbers
-	beq s2, s0, done
-	
+	beq s2, s0, result
+
 	la a0, prompt_value
 	li a7, SYS_printStr
 	ecall
 
 	li a7, SYS_readInt
-	ecall
+	ecall	# Input in a0
 
-	abs t0, a0	# Get the absolute value of value in a0, then store in t0
-	li t1 42
-	beq t0, t1, abs_counter
+	# Get absolute value
+	mv t0, a0
+	bgez t0, skip_abs	# If value >= 0, skip negation
+	neg t0, t0
 
-loop_counter:
+skip_abs:
+	# Check if number is 42
+	li t1, 42
+	bne t0, t1, skip_count
+	addi s1, s1, 1	# Increment s1 count
+
+skip_count:
+	# Increment loop counter and continue
 	addi s2, s2, 1
 	j loop
-	
 
-abs_counter:
-	addi s1, s1, 1
-	j loop
+result:
+	# Print result message
+	la a0, output_message
+	li a7, SYS_printStr
+	ecall
 
+	# Print result count
+	mv a0, s1
+	li a7, SYS_printInt
+	ecall
 
-done:
-	# Exit the program
+	la a0, newline
+	li a7, SYS_printStr
+	ecall
+
+exit:
 	li a0, 0
 	li a7, SYS_exit
 	ecall
