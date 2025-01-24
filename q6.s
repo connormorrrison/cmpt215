9d4629fffc49137a6189afd2875c8486108272a5
Connor Morrison <tvi340@usask.ca>
2025-01-23 22:16:47 -0600

    Continued implementation of q6.s
    - Add additional messages in 'read-only section'
    - Implemented search initialization section
    	- Prompt user for integer M
    	- Read integer M
    	- Reset array pointer / counter logic added

--- q6.s
+++ q6.s
@@ -6,7 +6,11 @@
 
 # Read-only data section
 	.section .rodata
-prompt_array: .asciz "Enter 10 integers: "
+prompt_array:      .asciz "Enter 10 integers: "
+prompt_m:          .asciz "Enter an integer M to search for: "
+found_message:     .asciz "Found at position: "
+not_found_message: .asicz "Not found.\n"
+newline:           .asciz "\n"
 
 # Uninitialized data section
 	.section .bss
@@ -18,10 +22,10 @@ array: .space 40	# 10 integers * 4 bytes each
 
 _start:
 	# Initialize array
-	la s0, array	# s0 is array pointer
-	li s1, 10
+	la s0, array	# s0 = array pointer
+	li s1, 10	# s1 = counter for number of elements
 
-loop:
+input_loop:
 	# Prompt for array
 	la a0, prompt_array
 	li a7, SYS_printStr
@@ -30,8 +34,28 @@ loop:
 	# Read integer
 	li a7, SYS_readInt
 	ecall
-	sw a0, 0(s0)	# Store at current pointer
-	addi s0, s0 4	# Move pointer to next position
+	sw a0, 0(s0)	# Store integer at current pointer
+	addi s0, s0, 4	# Move pointer to next position (move 4 bytes)
+	addi s1, s1, -1	# Decrement counter (effectively decrease available array size)
+	bnez s1, input_loop
+
+search_start:
+	# Prompt for input integer M
+	la a0, prompt_m
+	li a7, SYS_printStr
+	ecall
+
+	# Read integer M
+	li a7, SYS_readInt
+	ecall
+	mv s2, a0	# Move integer M from register a0 to s2
+
+	# Reset array pointer and element counter
+	la s0, array
+	li s1, 1
+
+search_loop:
+	li 
 
 exit:
 	li a0, 0
