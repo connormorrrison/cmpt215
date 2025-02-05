5a0ed44ca696ef19d1e8fe63f04603d4e8b54691
Connor Morrison <tvi340@usask.ca>
2025-02-05 16:55:16 -0600

    Continued implementation on q5.s
    - Reformatted _start to prompt for n first
    - Check for if n <= 25
    - Redoing array logic to increment up

--- q5.s
+++ q5.s
@@ -6,39 +6,76 @@
 
 # Read-only data section
 	.section .rodata
-prompt: .asciz "Enter up to 25 integers:\n"
-output: .asciz "Returned value: \n"
+prompt_n:      .asciz "Enter the number of elements (max 25): "
+prompt_number: .asciz "Enter number: "
+prompt_i:      .asciz "Enter i: "
+prompt_j:      .asciz "Enter j: "
+prompt_result: .asciz "Result: "
+newline:       .asciz "\n"
 
 # Uninitialized data section
 	.section .bss
-array:	.space 100
+array:	.space 100	# 25 integers * 4 bytes
 
 # Code section
 	.section .text
 	.globl _start
 
 _start: 
-	la a0, prompt
+	# Prompt for n
+	la a0, prompt_n
 	li a7, SYS_printStr
 	ecall
 	
-	la s0, array
-	li s1, 25
-
-loop:
+	# Read in n
 	li a7, SYS_readInt
 	ecall
+	mv s1, a0	# s0 = n
+	
+	# Ensure n <= 25
+	li t0, 25
+	bgt s1, t0, exit
+
+	# Else array is <= 25, and read ints into array
+	la s0, array
+	li s1, 0	# Start counter at 0
+
+read_number_loop:
+	# Read in number into array
+	li a7, SYS_readInt
 	sw a0, 0(s0)
-	addi s0, s0, 4
-	addi s1, s1, -1
-	bnez s1, loop
+	addi s0
 
-	addi s0, s0, -4
-	li s1, 25
-
-	la a0, output
-	li a7, SYS_printStr
+exit:
+	li a0, 0
+	li a7, SYS_exit
 	ecall
+
+
+
+
+
+	#la a0, prompt
+	#li a7, SYS_printStr
+	#ecall
+	
+	#la s0, array
+	#li s1, 25
+
+loop:
+	#li a7, SYS_readInt
+	#ecall
+	#sw a0, 0(s0)
+	#addi s0, s0, 4
+	#addi s1, s1, -1
+	#bnez s1, loop
+
+	#addi s0, s0, -4
+	#li s1, 25
+
+	#la a0, output
+	#li a7, SYS_printStr
+	#ecall
 
 	# Exit
 	li a0, 0
